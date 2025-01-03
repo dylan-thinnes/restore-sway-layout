5d99dcd1c8298b2b156baab9e02eb974d54b2aee
Dylan Thinnes <dylan.thinnes@protonmail.com>
2025-01-03 16:04:04 +0000

    Use asyncio unix socket functions

--- src/restore_sway_layout/main/ctl/__init__.py
+++ src/restore_sway_layout/main/ctl/__init__.py
@@ -5,8 +5,13 @@ import asyncio
 from dbus_fast import Message, MessageType
 from dbus_fast.aio import MessageBus
 
-async def dump_socket(sock):
-    async for line in socket_lines(sock):
+async def dump_socket(reader):
+    while True:
+        raw_line = await reader.readline()
+        if not raw_line:
+            break
+
+        line = raw_line.decode('utf-8').rstrip()
         print(line)
 
 async def async_main(args):
@@ -33,19 +38,19 @@ async def async_main(args):
         socket_file = args.socket_file
         print_stderr(f'Got socket file `{socket_file}` from args.')
 
-    with socket.socket(family=socket.AF_UNIX) as sock:
-        sock.connect(socket_file)
-        task = asyncio.create_task(dump_socket(sock))
-        if args.query_id:
-            sock.send(b'query-id\n')
-        elif args.echo is not None:
-            sock.send(b'echo\n' + args.echo.encode('utf-8') + b'\n')
-        elif args.update_snapshot:
-            sock.send(b'update-snapshot\n')
-        elif args.set_rate is not None:
-            sock.send(b'set-rate\n' + str(args.set_rate).encode('utf-8') + b'\n')
-        sock.shutdown(socket.SHUT_WR)
-        await task
+    reader, writer = await asyncio.open_unix_connection(socket_file)
+    task = asyncio.create_task(dump_socket(reader))
+    if args.query_id:
+        writer.write(b'query-id\n')
+    elif args.echo is not None:
+        writer.write(b'echo\n' + args.echo.encode('utf-8') + b'\n')
+    elif args.update_snapshot:
+        writer.write(b'update-snapshot\n')
+    elif args.set_rate is not None:
+        writer.write(b'set-rate\n' + str(args.set_rate).encode('utf-8') + b'\n')
+    writer.write_eof()
+    await writer.drain()
+    await task
 
 def main(args):
     return asyncio.run(async_main(args))
--- src/restore_sway_layout/main/daemon/__init__.py
+++ src/restore_sway_layout/main/daemon/__init__.py
@@ -28,32 +28,44 @@ async def snapshot_every(starting_duration, new_duration_queue):
         except asyncio.TimeoutError:
             pass
 
-async def handle_commands(sock, tasks, new_duration_queue):
-    line_generator = socket_lines(sock)
+async def handle_commands(reader, writer, tasks, new_duration_queue):
     command = None
-    async for line in line_generator:
+    while True:
+        raw_line = await reader.readline()
+        if not raw_line:
+            break
+
+        line = raw_line.decode('utf-8').rstrip()
+        if line == '':
+            continue
         if command is None:
             command = line
         if command == 'query-id':
             print_stderr(f'Query ID: {session_id}')
-            sock.send(session_id.encode('utf-8') + b'\n')
+            writer.write(session_id.encode('utf-8') + b'\n')
+            await writer.drain()
         elif command == 'echo':
-            print_stderr(f'Receiving line:')
-            received_line = await anext(line_generator)
-            sock.send(received_line.encode('utf-8') + b'\n')
-            print_stderr(received_line)
+            print_stderr(f'Receiving line...')
+            raw_line = await reader.readline()
+            line = raw_line.decode('utf-8').rstrip()
+            writer.write(line.encode('utf-8') + b'\n')
+            await writer.drain()
+            print_stderr(f'Received line: {line}')
         elif command == 'update-snapshot':
             print_stderr(f"Taking snapshot...")
             snapshot.save_snapshot(get_output_file())
             print_stderr(f"Done!")
         elif command == 'set-rate':
-            duration = float(await anext(line_generator))
+            raw_line = await reader.readline()
+            duration = float(raw_line.decode('utf-8').rstrip())
             print_stderr(f'Taking a snapshot every {duration}')
             new_duration_queue.put_nowait(duration)
         else:
             print_stderr(f'Command `{command}` unrecognized')
         command = None
-    sock.shutdown(socket.SHUT_WR)
+
+    writer.write_eof()
+    await writer.drain()
 
 def get_output_file():
     return os.path.join(output_dir, session_id + ".json")
@@ -85,29 +97,18 @@ async def async_main(args):
 
     with tempfile.TemporaryDirectory() as socket_dir:
         socket_file = os.path.join(socket_dir, f'restore-sway-layout-{session_id}-ipc.sock')
-        task = asyncio.create_task(setup_dbus(socket_file))
-        tasks.append(task)
+        dbus_task = asyncio.create_task(setup_dbus(socket_file))
         new_duration_queue = asyncio.Queue()
-        task = asyncio.create_task(snapshot_every(args.rate, new_duration_queue))
-        tasks.append(task)
-        with socket.socket(family=socket.AF_UNIX) as sock:
-            sock.bind(socket_file)
-            sock.listen()
-            if args.write_socket_to is not None:
-                with open(args.write_socket_to, 'w+') as fd:
-                    fd.write(socket_file)
-                print_stderr(f'Wrote socket file name `{socket_file}` into file `{args.write_socket_to}`')
-            else:
-                print(socket_file)
+        snapshot_task = asyncio.create_task(snapshot_every(args.rate, new_duration_queue))
+        unix_socket_task = asyncio.create_task(asyncio.start_unix_server(lambda reader, writer: handle_commands(reader, writer, tasks, new_duration_queue), socket_file))
 
-            await handle_connections(sock, tasks, new_duration_queue)
+        if args.write_socket_to is not None:
+            with open(args.write_socket_to, 'w+') as fd:
+                fd.write(socket_file)
+            print_stderr(f'Wrote socket file name `{socket_file}` into file `{args.write_socket_to}`')
+        else:
+            print(socket_file)
 
-    for task in tasks:
-        await task
-
-async def handle_connections(sock, tasks, new_duration_queue):
-    while True:
-        conn, conn_addr = await asyncio.to_thread(sock.accept)
-        print_stderr(f'Started connection {conn}')
-        task = asyncio.create_task(handle_commands(conn, tasks, new_duration_queue))
-        tasks.append(task)
+        await unix_socket_task
+        await snapshot_task
+        await dbus_task
